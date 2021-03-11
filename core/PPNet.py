9dab6afa86d6694b38e77e7f3e3458e187d5930b
minhna1112 <minhngau2804@gmail.com>
2021-03-11 19:11:36 +0700

    Build architecture for PPNet

---
+++ core/PPNet.py
@@ -0,0 +1,64 @@
+from core import common
+import tensorflow as tf
+from tensorboard.plugins.hparams import api as hp
+from tensorflow import keras
+
+def vector_route(input_layer, num_fc_blocks=1, increase=False, activation='leaky', dropout=0.5, init_num_units=8, final_units=64):
+    dense = common.fully_connected(input_layer, init_num_units, activate_type=activation, dropout=dropout)
+
+    for i in range(num_fc_blocks):
+        hidden_units = init_num_units
+        if increase:
+            hidden_units = init_num_units*(i+1)
+        dense = common.fully_connected(dense, hidden_units, activate_type=activation, dropout=dropout)
+
+    dense = common.fully_connected(dense, final_units, activate_type=activation, dropout=dropout)
+
+    return dense
+
+
+def image_route(input_layer, num_res_blocks=3, activation='leaky', dropout=0.1, init_num_features_map=32, final_feature_maps=64, use_pooling=True):
+    conv = common.convolutional(input_layer, filters_shape=(1,1, 3, init_num_features_map),activate_type=activation, dropout=dropout)
+    conv = common.convolutional(input_layer, filters_shape=(3, 3, 3, 2*init_num_features_map), activate_type=activation, dropout=dropout, downsample=True)
+
+    for i in range(num_res_blocks):
+        num_fm = (2**(i+1))*init_num_features_map
+        conv = common.residual_block(conv, num_fm, num_fm//2, num_fm, activate_type=activation, dropout=dropout)
+        conv = common.convolutional(conv, activate_type=activation, dropout=dropout, filters_shape=(1,1, num_fm, 2*num_fm), downsample=True)
+        if conv.shape[1] > 1:
+            if use_pooling:
+                conv = keras.layers.MaxPool2D()(conv)
+
+    conv = common.fully_conv_block(conv, init_num_features_map*(2**num_res_blocks), final_feature_maps, activate_type=activation, dropout=dropout)
+
+    return conv
+
+
+    return conv
+
+def PPNet(input_image, input_vector, num_fc_blocks=2, num_res_blocks=2, fc_activation='leaky', conv_activation='leaky', init_num_units=16, final_num_units=64, fc_dropout=0.5, conv_dropout=0.1):
+    vector_output = vector_route(input_vector, num_fc_blocks, fc_activation, fc_dropout, init_num_units=init_num_units, final_units=final_num_units)
+    image_output = image_route(input_image, num_res_blocks, conv_activation, conv_dropout, init_num_features_map=init_num_units, final_feature_maps=final_num_units)
+
+    print(vector_output.shape)
+    print(image_output.shape)
+
+    assert vector_output.shape[-1] == image_output.shape[-1]
+
+    image_output = keras.layers.Flatten()(image_output)
+    net_output = tf.concat([vector_output, image_output], axis=-1)
+    print(net_output.shape)
+
+    net_output = common.fully_connected(net_output, units=16, activate_type=fc_activation, dropout=fc_dropout)
+    net_output = common.fully_connected(net_output, units=3, activate=False)
+
+    return net_output
+
+def create_net():
+    input_vector = keras.layers.Input(shape=(4,), name='input_vector')
+    input_image = keras.layers.Input(shape=(416,416,3), name='input_image')
+    net_output = PPNet(input_image, input_vector)
+    model = keras.Model(inputs=[input_image, input_vector], outputs=net_output, name='PPNet')
+
+
+
--- core/common.py
+++ core/common.py
@@ -1,3 +1,78 @@
+import tensorflow as tf
+from tensorflow import keras
 
-print('init')
+
+def fully_connected(input_layer, units, activate=True, bn=True, activate_type='leaky', dropout=None):
+    dense = keras.layers.Dense(units)(input_layer)
+    print(dense)
+    if bn:
+        dense = keras.layers.BatchNormalization()(dense)
+    if activate:
+        if activate_type == 'leaky':
+            dense = tf.nn.leaky_relu(dense, alpha=0.1)
+        elif activate_type == "mish":
+            dense = mish(dense)
+        elif activate_type == "relu":
+            dense = tf.nn.relu(dense)
+    if dropout is not None:
+        dense = keras.layers.Dropout(dropout)(dense)
+    print(dense)
+    return dense
+
+
+def convolutional(input_layer, filters_shape, downsample=False, activate=True, bn=True, activate_type='leaky',
+                  dropout=None, flatten=False):
+    strides = 1
+    if downsample:
+        padding = 'valid'
+        if not flatten:
+            strides = 2
+            #input_layer = keras.layers.ZeroPadding2D(((1, 0), (1, 0)))(input_layer)
+    else:
+        padding = 'same'
+
+    conv = keras.layers.Conv2D(filters=filters_shape[-1], kernel_size=(filters_shape[0], filters_shape[1]), strides=strides,
+                               padding=padding,
+                               use_bias=not bn, kernel_regularizer=keras.regularizers.l2(0.0005),
+                               kernel_initializer=tf.random_normal_initializer(stddev=0.01),
+                               bias_initializer=tf.constant_initializer(0.))(input_layer)
+
+    if bn:
+        conv = keras.layers.BatchNormalization()(conv)
+
+    if activate:
+        if activate_type == "leaky":
+            conv = tf.nn.leaky_relu(conv, alpha=0.1)
+        elif activate_type == "mish":
+            conv = mish(conv)
+        elif activate_type == "relu":
+            conv = tf.nn.relu(conv)
+
+    if dropout is not None:
+        conv = keras.layers.Dropout(dropout)(conv)
+    return conv
+
+
+def mish(x):
+    return x * tf.math.tanh(tf.math.softplus(x))
+
+
+def residual_block(input_layer, input_channel, filter_num1, filter_num2, activate_type='leaky', dropout=0.1):
+    short_cut = input_layer
+    conv = convolutional(input_layer, filters_shape=(1, 1, input_channel, filter_num1), activate_type=activate_type,
+                         dropout=dropout)
+    conv = convolutional(conv, filters_shape=(3, 3, filter_num1, filter_num2), activate_type=activate_type,
+                         dropout=dropout)
+
+    residual_output = short_cut + conv
+    return residual_output
+
+
+def fully_conv_block(input_layer, input_channel, filter_num1, activate_type='leaky', dropout=0.5):
+    conv = convolutional(input_layer,
+                         filters_shape=(input_layer.shape[1], input_layer.shape[2], input_channel, input_channel//2),
+                         activate_type=activate_type, downsample=True, flatten=True, dropout=dropout)
+    conv = convolutional(conv, filters_shape=(1, 1, input_channel//2, filter_num1), activate_type=activate_type,
+                         flatten=True, dropout=dropout)
+    return conv
 
