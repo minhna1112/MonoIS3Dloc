5578924345177a198b8fd54d4aefdfc57ff335a4
minhna1112 <minhngau2804@gmail.com>
2021-05-21 03:12:52 +0700

    Optimize training.py and evluation.py

--- training.py
+++ training.py
@@ -1,88 +1,65 @@
-import numpy as np
-import faulthandler;
-
-import evaluate
-
-faulthandler.enable()
 import tensorflow as tf
 from core import PPNet
-import pandas as pd
-#import matplotlib.pyplot as plt
+from core import dataset
+import evaluate
 
-def normalize_output(df):
-    df['x'] = df['x'].div(100)
-    df['y'] = df['y'].div(100)
-    df['z'] = df['z'].div(100)
+import datetime
+import os
 
-    return df
+today = datetime.datetime.today()
 
-def train():
-    input_shape = (224,224,1)
-    train_path = './data/train.csv'
-    train_df = pd.read_csv(train_path)
-    train_df = normalize_output(train_df)
 
-    val_path = './data/val.csv'
-    val_df = pd.read_csv(val_path)
-    val_df = normalize_output(val_df)
+def train(train_data, val_data, train_option='scratch', network_resolution=(224, 224, 1), pretrained_model_path=None, save_model_path=None,  log_path = None):
+    STEP_SIZE_TRAIN=train_data.n//train_data.batch_size
+    STEP_SIZE_VAL=val_data.n//val_data.batch_size
 
-    #Data generator
-    #data_gen = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
-    data_gen = tf.keras.preprocessing.image.ImageDataGenerator()
+    append = False
+    
+    if save_model_path is None:
+        save_model_path = './weights/{y}-{m}-{d}-{h}:{min}'.format(y = today.year, m = today.month, d = today.day, 
+                                                                   h = today.hour, min = today.minute)
+    if log_path is None:
+        log_path = os.path.join('logs', save_model_path.split('/')[-1] + '.log')
 
-    train_generator = data_gen.flow_from_dataframe(dataframe=train_df,
-                                 directory='./data/train',
-                                 x_col='img',
-                                 y_col=['x', 'y', 'z'],
-                                 target_size=(input_shape[0], input_shape[1]),
-                                 color_mode='grayscale',
-                                 class_mode='raw',
-                                 batch_size=8
-                                 )
+    if train_option is not 'scratch':
+        #Transfer or continue
+        model = tf.keras.models.load_model(pretrained_model_path, compile=False)
+        if train_option == 'continue':
+            append = True
+            log_path = os.path.join('logs', pretrained_model_path.split('/')[-1] + '.log')
+    else:
+        #Train from scratch
+        model = PPNet.create_simple_net(network_resolution, init_num_fm=32, conv_dropout=0, fc_dropout=0.25, turn_off_later_bn=True, conv_activation='relu', fc_activation='relu')
 
-    val_generator = data_gen.flow_from_dataframe(dataframe=val_df,
-                                 directory='./data/val',
-                                 x_col='img',
-                                 y_col=['x', 'y', 'z'],
-                                 target_size=(input_shape[0], input_shape[1]),
-                                 color_mode='grayscale',
-                                 class_mode='raw',
-                                 batch_size=16
-                                 )
-    #Define number of iterations in each epoch = number of samples / batch_size
-    STEP_SIZE_TRAIN=train_generator.n//train_generator.batch_size
-    STEP_SIZE_VAL=val_generator.n//val_generator.batch_size
-
-    #Create model
-    #model = PPNet.create_net(input_shape, num_csp_blocks=4)
-    model = PPNet.create_simple_net(input_shape, init_num_fm=32)
 
     optimizer = tf.keras.optimizers.Adam(learning_rate=1e-4)
-    model.compile(loss=PPNet.CustomRMSE(), optimizer=optimizer)
     model.compile(loss='mse', optimizer=optimizer, metrics=[evaluate.RelativeError()])
-    csv_logger = tf.keras.callbacks.CSVLogger('training_1505_downscaled_mse_bigset-1e-4init32.log')
+    model.summary()
 
-    history = model.fit(train_generator,
+    csv_logger = tf.keras.callbacks.CSVLogger(log_path, append=append)
+
+    history = model.fit(train_data,
                         steps_per_epoch=STEP_SIZE_TRAIN,
-                        validation_data=val_generator,
+                        validation_data=val_data,
                         validation_steps=STEP_SIZE_VAL,
                         validation_freq=1,
-                        epochs=10,
+                        epochs=20,
                         callbacks=[csv_logger])
 
-    model.save('./weights/training_1505_downscaled_mse_bigset-1e-4init32')
+
+
+    model.save(save_model_path)
 
     return  history.history['loss'],  history.history['val_loss'], history.history['val_relative_error']
 
-
-'''def plot_learning_curve(loss, val_loss):
-    plt.figure()
-    plt.plot(range(len(loss)), [l for l in loss])
-    plt.plot(range(len(val_loss)), [l for l in val_loss])
-    plt.show()
-    plt.savefig('new_curve.png')
-'''
-
 if __name__ == '__main__':
-    loss, val_loss, val_rel = train()
-    #plot_learning_curve(loss, val_loss)
+    
+    data_gen = tf.keras.preprocessing.image.ImageDataGenerator()
+    train_data = dataset.DataLoader('../IVSR_BINARY_DATA/10imperpose/data/', data_gen, split='train')
+    val_data = dataset.DataLoader('../IVSR_BINARY_DATA/10imperpose/data/', data_gen, split='val')
+
+    train_batch = train_data.batch_loader()
+    val_batch = val_data.batch_loader()
+    
+    loss, val_loss, val_rel = train(train_data=train_batch, val_data=val_batch, train_option='continue', pretrained_model_path='./weights/training_1505_downscaled_mse_10impperpose-1e-4init32')
+    
