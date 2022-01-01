f137cd525448fe2b4c4694da9de5c2215ed23e80
ivsr <ivsr.set.hust@gmail.com>
2022-01-02 02:23:31 +0700

    Reqrite loss, model, dataset into custom subclass pradigm

---
+++ data/dataset.py
@@ -0,0 +1,46 @@
+import tensorflow as tf
+import pandas as pd
+
+MAX_VALUE = 30.0
+
+input_shape = (398, 224, 1)
+
+train_path = "/home/ivsr/CV_Group/phuc/airsim/train.csv"
+train_df = pd.read_csv(train_path)
+train_df["x"] = train_df["x"].div(MAX_VALUE)
+train_df["y"] = train_df["y"].div(MAX_VALUE)
+train_df["z"] = train_df["z"].div(MAX_VALUE)
+val_path = "/home/ivsr/CV_Group/phuc/airsim/val.csv"
+val_df = pd.read_csv(val_path)
+val_df["x"] = val_df["x"].div(MAX_VALUE)
+val_df["y"] = val_df["y"].div(MAX_VALUE)
+val_df["z"] = val_df["z"].div(MAX_VALUE)
+
+data_gen = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
+
+train_loader = data_gen.flow_from_dataframe(
+      dataframe=train_df,
+      directory="/home/ivsr/CV_Group/phuc/airsim/data",
+      x_col="img",
+      y_col=["x", "y", "z"],
+      target_size=(input_shape[1], input_shape[0]),
+      color_mode="grayscale",
+      class_mode="raw",
+      batch_size=32)
+
+val_loader = data_gen.flow_from_dataframe(
+        dataframe=val_df,
+        directory="/home/ivsr/CV_Group/phuc/airsim/data",
+        x_col="img",
+        y_col=["x", "y", "z"],
+        target_size=(input_shape[1], input_shape[0]),
+        color_mode="grayscale",
+        class_mode="raw",
+        batch_size=1)
+
+STEP_SIZE_TRAIN = train_loader.n//train_loader.batch_size
+STEP_SIZE_VAL = val_loader.n//val_loader.batch_size
+
+next_batch = next(iter(val_loader))
+print(next_batch[0].shape) #
+print(next_batch[1].shape)
--- model/model.py
+++ model/model.py
@@ -11,7 +11,7 @@ class SimpleNet(tf.keras.Model):
         self.image_shape = image_shape
         self.activation  = activation
 
-        def downsample_convolution(out_channels=32, input_shape=None, name: str):
+        def downsample_convolution(out_channels=32, input_shape=None, name='conv'):
             if input_shape is not None:
                 conv = Conv2D(out_channels, (3, 3), activation=self.activation, input_shape=input_shape)
             else:
@@ -20,15 +20,17 @@ class SimpleNet(tf.keras.Model):
 
             return Sequential([conv, pool], name=name)
 
-        self.conv1 = downsample_convolution(32, self.image_shape)
-        self.conv2 = downsample_convolution(64)
-        self.conv3 = downsample_convolution(128)
-        self.conv4 = downsample_convolution(256)
-        self.conv5 = downsample_convolution(256)
+        self.conv1 = downsample_convolution(32, name='conv1')
+        self.conv2 = downsample_convolution(64, name='conv2')
+        self.conv3 = downsample_convolution(128, name='conv3')
+        self.conv4 = downsample_convolution(256, name='conv4')
+        self.conv5 = downsample_convolution(256, name='conv5')
+        self.conv6 = downsample_convolution(256, name='conv6')
 
         self.flat = Flatten()
-        self.dense1 = Dense(512, activation=self.activation)
-        self.dense2 = Dense(64, activation=self.activation)
+        self.dense1 = Dense(512, activation=self.activation, name='dense1')
+        self.dense2 = Dense(64, activation=self.activation, name='dense2')
+        self.dense3 = Dense(3, activation='tanh', name='out')
 
     def call(self, inputs, training=None, mask=None):
         out = self.conv1(inputs)
@@ -36,12 +38,63 @@ class SimpleNet(tf.keras.Model):
         out= self.conv3(out)
         out = self.conv4(out)
         out = self.conv5(out)
-
+        #print(out.shape)
+        out = self.conv6(out)
+        #print(out.shape)
         out = self.flat(out)
+        #print(out.shape)
         out = self.dense1(out)
-        out = (out)
-        out = Dense(3, activation='tanh')(out)
+        out = self.dense2(out)
+        out = self.dense3(out)
         return out
+
+class DepthAwareNet(tf.keras.Model):
+    def __init__(self, activation='relu',input_shape=(224,398,1)):
+        super().__init__()
+        self.activation  = activation
+
+        def downsample_convolution(out_channels=32, input_shape=None, name='conv'):
+            if input_shape is not None:
+                conv = Conv2D(out_channels, (3, 3), activation=self.activation, input_shape=input_shape)
+            else:
+                conv = Conv2D(out_channels, (3, 3), activation=self.activation)
+            pool = MaxPooling2D(pool_size=(2, 2))
+
+            return Sequential([conv, pool], name=name)
+
+        self.conv1 = downsample_convolution(32, name='conv1')
+        self.conv2 = downsample_convolution(64, name='conv2')
+        self.conv3 = downsample_convolution(128, name='conv3')
+        self.conv4 = downsample_convolution(256, name='conv4')
+        self.conv5 = downsample_convolution(256, name='conv5')
+        self.conv6 = downsample_convolution(256, name='conv6')
+
+        self.flat = Flatten()
+        self.dense1 = Dense(512, activation=self.activation, name='dense1')
+        self.dense2 = Dense(64, activation=self.activation, name='dense2')
+        self.dense3 = Dense(3, name='dense3')
+        self.sigmoid = tf.keras.layers.Activation('sigmoid')
+        self.tanh = tf.keras.layers.Activation('tanh')
+
+    def call(self, inputs, training=None, mask=None):
+        out = self.conv1(inputs)
+        out = self.conv2(out)
+        out= self.conv3(out)
+        out = self.conv4(out)
+        out = self.conv5(out)
+        #print(out.shape)
+        out = self.conv6(out)
+        #print(out.shape)
+        out = self.flat(out)
+        #print(out.shape)
+        out = self.dense1(out)
+        out = self.dense2(out)
+        out = self.dense3(out)
+        x, y, z = tf.split(value=out, num_or_size_splits=3, axis=-1)
+        x = self.sigmoid(x)
+        y = self.tanh(y)
+        z = self.tanh(z)
+        return x,y,z #[(batch_size, 1), (batch_size, 1), (batch_size, 1)]
 
 
 def simple_net(image_shape=(224, 398, 1)):
@@ -68,9 +121,23 @@ def simple_net(image_shape=(224, 398, 1)):
     return model
 
 if __name__ == '__main__':
-    model1 = simple_net()
-    model1.summary()
+    #model1 = simple_net()
+    #model1.summary()
 
-    model2 = SimpleNet(image_shape=(224,398,3))
-    model2.build(input_shape=(None,224,398,3))
-    model2.summary()
+    # model2 = SimpleNet(image_shape=(224,398,1))
+    # model2.build(input_shape=(None,224,398,1))
+    # model2.summary()
+    #
+    #
+    # x = tf.ones(shape=(4, 224,398, 1))
+    # #assert model2(x).shape == model1(x).shape
+    # print(model2(x).shape) #(Batch_size, 3)
+    #
+
+    model3 = DepthAwareNet()
+    model3.build(input_shape=(None, 224, 398, 1))
+    model3.summary()
+
+    x = tf.ones(shape=(4, 224, 398, 1))
+    # assert model2(x).shape == model1(x).shape
+    print(model3(x))  # [(Batch_size, 1), (Batch_size, 1),(Batch_size, 1)[
